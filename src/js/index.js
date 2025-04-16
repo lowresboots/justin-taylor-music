8512fb70c26f445525353c142b53de25c3090ad3
lowresboots <lowresboots@gmail.com>
2025-04-16 02:47:05 -0400

    Initial project setup with webpack and hero section

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+// Import styles
+import '../styles/main.scss';
+
+// Import assets
+import guitarSvg from '../assets/images/cguitar_cleaned.svg';
+
+// Load the guitar SVG into the DOM when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  const logoContainer = document.querySelector('.logo');
+  if (logoContainer) {
+    // Create an img element for the SVG
+    const img = document.createElement('img');
+    img.src = guitarSvg;
+    img.alt = 'Justin Taylor Music Guitar Logo';
+    img.className = 'guitar-logo';
+    
+    // Append to the logo container
+    logoContainer.appendChild(img);
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
