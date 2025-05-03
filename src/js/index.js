6cd286ab8161f80d11c037e0300638bbc1a2d653
lowresboots <lowresboots@gmail.com>
2025-05-03 06:38:45 -0400

    Create mug/merch section

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../styles/main.scss';
 
 // Import assets
 import guitarSvg from '../assets/images/cguitar_cleaned.svg';
+import mugImg from '../assets/images/mug.png';
 
 // Load the guitar SVG into the DOM when the page loads
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,8 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
     img.src = guitarSvg;
     img.alt = 'Justin Taylor Music Guitar Logo';
     img.className = 'guitar-logo';
-    
+
     // Append to the logo container
     logoContainer.appendChild(img);
+  }
+
+  const mugImage = document.querySelector('.mug-image');
+  if (mugImage) {
+    mugImage.src = mugImg;
   }
 });
